9d931f969c832ac4290574a3360e20a65c4adee3
Shahar Best <bestshahar9@gmail.com>
2024-03-04 11:43:05 +0200

    public middleware

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const publicPath = path.join(__dirname, 'public');
+
 app.use(require('cors')());
 app.use(express.json());
+app.use(express.static(publicPath));
 
 app.get('/ping', (req, res) => {
 	res.send('pong');
---
+++ public/script.js
@@ -0,0 +1,3 @@
+// const nisim = document.querySelector('');
+// nisim.addEventListener('click', handler);
+// console.log('hi');
