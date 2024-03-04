535e43ca666baba0d8d3000f9dd16193abf33790
Shahar Best <bestshahar9@gmail.com>
2024-03-04 11:28:25 +0200

    cors and port in .env

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(require('cors')());
 app.use(express.json());
 
 app.get('/ping', (req, res) => {
